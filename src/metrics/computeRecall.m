b0143ef8920ff4ab0c2d3ddb7c05847193f17b08
Cole Shaigec <coleshaigec@Coles-MacBook-Air-3.local>
2026-04-18 19:10:44 -0400

    Implement utility to compute recall

---
+++ src/metrics/computeRecall.m
@@ -0,0 +1,32 @@
+function recall = computeRecall(yHat, yTrue, warningHorizon)
+    % COMPUTERECALL Computes recall for classification model.
+    %
+    % AUTHOR: Cole H. Shaigec
+    %
+    % INPUTS
+    %  yHat (n x 1 double)                - predicted labels (+1 or -1)
+    %  yTrue (n x 1 double)               - true RUL values
+    %  warningHorizon (positive integer)  - TTF threshold for classification
+    %
+    % OUTPUT
+    %  recall (double)
+
+    % -- Actual positives --
+    actualPositives = (yTrue <= warningHorizon);
+
+    % -- Predicted positives --
+    predictedPositives = (yHat == 1);
+
+    % -- Handle edge case cleanly --
+    if sum(actualPositives) == 0
+        recall = 0;
+        return;
+    end
+
+    % -- True positives --
+    truePositives = predictedPositives & actualPositives;
+
+    % -- Compute recall --
+    recall = sum(truePositives) / (sum(actualPositives));
+
+end
