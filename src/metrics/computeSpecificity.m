d458082a234a309a408bcbe8c9a9ceab38daec66
Cole Shaigec <coleshaigec@Coles-MacBook-Air-3.local>
2026-04-18 19:29:42 -0400

    Implement utility to compute specificity

---
+++ src/metrics/computeSpecificity.m
@@ -0,0 +1,34 @@
+function specificity = computeSpecificity(yHat, yTrue, warningHorizon)
+    % COMPUTESPECIFICITY Computes specificity for classification model.
+    %
+    % AUTHOR: Cole H. Shaigec
+    %
+    % INPUTS
+    %  yHat (n x 1 double)                - predicted labels (+1 or -1)
+    %  yTrue (n x 1 double)               - true RUL values
+    %  warningHorizon (positive integer)  - TTF threshold for classification
+    %
+    % OUTPUT
+    %  specificity (double)
+
+    % specificity = TN / (TN + FP)
+
+    % -- Actual negatives --
+    actualNegatives = yTrue > warningHorizon;
+    
+    % -- Handle edge case cleanly --
+    numActualNegatives = sum(actualNegatives);
+    if numActualNegatives == 0
+        specificity = 0;
+        return;
+    end
+
+    % -- Predicted negatives --
+    predictedNegatives = yHat == -1;
+
+    % -- True negatives --
+    trueNegatives = actualNegatives & predictedNegatives;
+    numTrueNegatives = sum(trueNegatives);
+
+    specificity = numTrueNegatives / numActualNegatives;
+end
