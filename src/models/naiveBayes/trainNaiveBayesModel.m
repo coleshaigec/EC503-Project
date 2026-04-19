b97a7ae8f48f3420e168d122c49c634fb36001f2
Cole Shaigec <coleshaigec@crc-dot1x-nat-10-239-73-95.bu.edu>
2026-04-19 11:10:46 -0400

    Implement Naive Bayes model trainer

--- src/models/naiveBayes/trainNaiveBayesModel.m
+++ src/models/naiveBayes/trainNaiveBayesModel.m
@@ -19,8 +19,38 @@ function naiveBayesModel = trainNaiveBayesModel(trainingData, naiveBayesHyperpar
     %      .classFeatureMeans (K x d double)      - class-conditional feature means
     %      .classFeatureVariances (K x d double)  - regularized class-conditional feature variances
     %      .varianceSmoothing (double >= 0)       - variance regularization term
+    
+    labels = unique(trainingData.y);
+    numClasses = numel(labels);
 
+    epsilon = naiveBayesHyperparameters.varianceSmoothing;
+    [n, d] = size(trainingData.X);
+    priors = zeros(numClasses, 1);
+    classFeatureMeans = zeros(numClasses, d); % store each class mean as a row vector
+    classFeatureVariances = zeros(numClasses, d);
+
+    for i = 1 : numClasses
+        currentLabel = labels(i);
+
+        % -- Estimate class priors --
+        Ik = trainingData.y == currentLabel;
+        nk = sum(Ik);
+        priors(i) = nk / n;
+
+        % -- Estimate class feature means --
+        samplesOfCurrentClass = trainingData.X(Ik,:);
+        classFeatureMeans(i, :) = mean(samplesOfCurrentClass, 1);
+        classFeatureVariances(i, :) = var(samplesOfCurrentClass, 1, 1) + epsilon;
+    end
+
+    % -- Populate output struct --
     naiveBayesModel = struct();
+    naiveBayesModel.classLabels = labels;
+    naiveBayesModel.classPriors = priors;
+    naiveBayesModel.logClassPriors = log(priors);
+    naiveBayesModel.classFeatureMeans = classFeatureMeans;
+    naiveBayesModel.classFeatureVariances = classFeatureVariances;
+    naiveBayesModel.varianceSmoothing = epsilon;
 
     % -- Output validation - PLEASE DO NOT REMOVE --
     validateNaiveBayesModel( ...
