efaf422d4164d1a1d62a34a913f358c60d10bbbf
Cole Shaigec <coleshaigec@crc-dot1x-nat-10-239-73-95.bu.edu>
2026-04-22 18:17:45 -0400

    Wind down Cole-prelim branch

--- src/reporting/analyzeAndReportExperimentResults.m
+++ src/reporting/analyzeAndReportExperimentResults.m
@@ -4,6 +4,11 @@ function analyzeAndReportExperimentResults(runReports)
     % AUTHOR: Cole H. Shaigec
 
     % Tasks of this utility
+    % GAME PLAN HAS CHANGED!
+    % - Drop plotting
+    % - Don't worry about best runs, let Excel handle it
+    % - Don't worry about plotting, let Excel handle it
+    % - Run policy analysis FOR ALL RUNS
     % 1. Build summary table of all run results
     % 2. Choose best runs according to some criterion
     % 3. Compute more elaborate suite of metrics and visualize results for
@@ -15,18 +20,19 @@ function analyzeAndReportExperimentResults(runReports)
     % models
 
     % -- Build summary table --
+    policyAnalysisResults = runPolicyAnalysis(runReports);
     summaryTable = buildExperimentSummaryTable(runReports);
     writeSummaryTableToFile(summaryTable, getCSVOutFileName());
 
     % -- Choose best runs and analyze them in more depth --
-    bestRuns = chooseBestRunsFromExperiment(summaryTable, 3);
-    bestRunReportsClassification = runReports(bestRuns.indices.classification);
-    bestRunReportsRegression = runReports(bestRuns.indices.regression);
-    bestRunReports = struct();
-    bestRunReports.classification = bestRunReportsClassification;
-    bestRunReports.regression = bestRunReportsRegression;
-
-    % bestRunReports = runReports();
+    % bestRuns = chooseBestRunsFromExperiment(summaryTable, 3);
+    % bestRunReportsClassification = runReports(bestRuns.indices.classification);
+    % bestRunReportsRegression = runReports(bestRuns.indices.regression);
+    % bestRunReports = struct();
+    % bestRunReports.classification = bestRunReportsClassification;
+    % bestRunReports.regression = bestRunReportsRegression;
+    % 
+    % % bestRunReports = runReports();
 
     
 end
