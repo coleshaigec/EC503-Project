11fd822a07b62de37b1461cd005fab8ca6de0455
Cole Shaigec <153002396+coleshaigec@users.noreply.github.com>
2026-05-01 19:43:39 -0400

    Delete unused files and clean up directories

--- quarantine/validateDatasetSpec.m
+++
@@ -1,7 +0,0 @@
-function validateDatasetSpec(datasetSpec)
-    % Function stub for dataset validation
-    POSITIVE_INTEGER_ATTRIBUTES = {'scalar', 'finite', 'positive', 'integer'};
-    
-    
-    validateattributes(spec.id, {'numeric'}, POSITIVE_INTEGER_ATTRIBUTES, mfilename, 'spec.id');
-end
--- quarantine/validateSingleRunDataset.m
+++
@@ -1,4 +0,0 @@
-function validateSingleRunDataset(rawData)
-   % STUB -- all dataset validation should run through here instead of
-   % constant checks in lower-level function calls
-end
